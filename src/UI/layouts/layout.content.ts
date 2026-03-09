05b00b2c9df96ccba5f14f03e2a3e37a75a7cb15
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2026-03-09 21:06:32 +0100

    Put content in content file 🧼

---
+++ src/UI/layouts/layout.content.ts
@@ -0,0 +1,6 @@
+export const layoutContent = Object.freeze({
+  header: 'JMDK',
+  siteTitle: 'Jannik Maag - Software Engineer',
+  metaDescription:
+    'Jannik Maag is a Senior Software Engineer and Front-End architect. He specialises in writing testable framework agnostic solutions at scale.',
+});
--- src/UI/pages/index.content.ts
+++ src/UI/pages/index.content.ts
@@ -1,5 +1,4 @@
 export const indexContent = Object.freeze({
-  header: 'JMDK',
   heading: {
     text: 'Jannik Maag',
     dot: '.',
