be9f7bc8bb018f83da074fbc5eb45000031f57b2
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2026-03-09 20:02:51 +0100

    Add E2E tests 🧪

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src',
+  testMatch: /.*.spec.ts/,
+  fullyParallel: true,
+  retries: 0,
+  workers: 4,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5183',
+    trace: 'on-first-retry',
+    testIdAttribute: 'data-test',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: [
+    {
+      command: 'npm run dev:e2e',
+      url: 'http://localhost:5183/',
+    },
+  ],
+});
---
+++ src/E2E/website.spec.ts
@@ -0,0 +1,45 @@
+import { JMDKTestHarness } from './website.test-harness';
+import { test } from 'playwright/test';
+
+test('renders the website', async ({ page }) => {
+  const jmdk = new JMDKTestHarness(page);
+  await jmdk.goToRoot();
+  await jmdk.isOnMainPage();
+});
+
+test.describe('front page navigation', () => {
+  test('goes to my GitHub page in a new tab when clicking on "GitHub"', async ({
+    page,
+  }) => {
+    const jmdk = new JMDKTestHarness(page);
+    const browserTab = jmdk.useNewBrowserTab();
+    await jmdk.goToRoot();
+    await jmdk.clickOnGitHub();
+    await browserTab.waitForNewTabToOpen({
+      url: 'https://github.com/fuzzypawzz',
+      title: 'fuzzypawzz (Jannik Maag) · GitHub',
+    });
+  });
+
+  test('goes to my LinkedIn profile in a new tab when clicking on "LinkedIn"', async ({
+    page,
+  }) => {
+    const jmdk = new JMDKTestHarness(page);
+    const browserTab = jmdk.useNewBrowserTab();
+    await jmdk.goToRoot();
+    await jmdk.clickOnLinkedIn();
+    await browserTab.isOnLinkedInFrontPage();
+  });
+
+  test('goes to Storybook in a new tab when clicking on "Storybook"', async ({
+    page,
+  }) => {
+    const jmdk = new JMDKTestHarness(page);
+    const browserTab = jmdk.useNewBrowserTab();
+    await jmdk.goToRoot();
+    await jmdk.clickOnStorybook();
+    await browserTab.waitForNewTabToOpen({
+      url: 'https://jmdkstorybook.netlify.app/',
+    });
+  });
+});
