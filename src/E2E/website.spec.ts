420bea591cb2069a86b6212a06724e075744098d
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2026-03-09 20:07:04 +0100

    Place UI files in /UI 📦

--- src/E2E/website.spec.ts
+++ src/E2E/website.spec.ts
@@ -1,8 +1,8 @@
-import { JMDKTestHarness } from './website.test-harness';
+import { WebsiteTestHarness } from './website.test-harness';
 import { test } from 'playwright/test';
 
 test('renders the website', async ({ page }) => {
-  const jmdk = new JMDKTestHarness(page);
+  const jmdk = new WebsiteTestHarness(page);
   await jmdk.goToRoot();
   await jmdk.isOnMainPage();
 });
@@ -11,7 +11,7 @@ test.describe('front page navigation', () => {
   test('goes to my GitHub page in a new tab when clicking on "GitHub"', async ({
     page,
   }) => {
-    const jmdk = new JMDKTestHarness(page);
+    const jmdk = new WebsiteTestHarness(page);
     const browserTab = jmdk.useNewBrowserTab();
     await jmdk.goToRoot();
     await jmdk.clickOnGitHub();
@@ -24,7 +24,7 @@ test.describe('front page navigation', () => {
   test('goes to my LinkedIn profile in a new tab when clicking on "LinkedIn"', async ({
     page,
   }) => {
-    const jmdk = new JMDKTestHarness(page);
+    const jmdk = new WebsiteTestHarness(page);
     const browserTab = jmdk.useNewBrowserTab();
     await jmdk.goToRoot();
     await jmdk.clickOnLinkedIn();
@@ -34,7 +34,7 @@ test.describe('front page navigation', () => {
   test('goes to Storybook in a new tab when clicking on "Storybook"', async ({
     page,
   }) => {
-    const jmdk = new JMDKTestHarness(page);
+    const jmdk = new WebsiteTestHarness(page);
     const browserTab = jmdk.useNewBrowserTab();
     await jmdk.goToRoot();
     await jmdk.clickOnStorybook();
--- src/E2E/website.test-harness.ts
+++ src/E2E/website.test-harness.ts
@@ -1,60 +1,60 @@
-import { type Page, expect } from '@playwright/test'
+import { type Page, expect } from '@playwright/test';
 
-export class JMDKTestHarness {
-  readonly page
-  readonly mainPageHeading
-  readonly githubButton
-  readonly linkedInButton
-  readonly storybookButton
+export class WebsiteTestHarness {
+  readonly page;
+  readonly mainPageHeading;
+  readonly githubButton;
+  readonly linkedInButton;
+  readonly storybookButton;
 
   constructor(page: Page) {
-    this.page = page
-    this.mainPageHeading = page.getByText('Jannik Maag')
-    this.githubButton = page.getByRole('button', { name: 'My GitHub' })
-    this.storybookButton = page.getByRole('button', { name: 'Storybook' })
+    this.page = page;
+    this.mainPageHeading = page.getByText('Jannik Maag');
+    this.githubButton = page.getByRole('button', { name: 'My GitHub' });
+    this.storybookButton = page.getByRole('button', { name: 'Storybook' });
     this.linkedInButton = page.getByRole('button', {
       name: 'My profile on LinkedIn',
-    })
+    });
   }
 
   async goToRoot() {
-    await this.page.goto('/')
+    await this.page.goto('/');
   }
 
   async isOnMainPage() {
-    await expect(this.mainPageHeading).toBeVisible()
+    await expect(this.mainPageHeading).toBeVisible();
   }
 
   async clickOnGitHub() {
-    await this.githubButton.click()
+    await this.githubButton.click();
   }
 
   async clickOnStorybook() {
-    await this.storybookButton.click()
+    await this.storybookButton.click();
   }
 
   async clickOnLinkedIn() {
-    await this.linkedInButton.click()
+    await this.linkedInButton.click();
   }
 
   useNewBrowserTab() {
-    const newTabPromise = this.page.waitForEvent('popup')
+    const newTabPromise = this.page.waitForEvent('popup');
 
     return {
       async waitForNewTabToOpen(args: { url: string; title?: string }) {
-        const newTab = await newTabPromise
-        await newTab.waitForLoadState()
-        await expect(newTab).toHaveURL(args.url)
-        if (args.title) await expect(newTab).toHaveTitle(args.title)
+        const newTab = await newTabPromise;
+        await newTab.waitForLoadState();
+        await expect(newTab).toHaveURL(args.url);
+        if (args.title) await expect(newTab).toHaveTitle(args.title);
       },
 
       async isOnLinkedInFrontPage() {
-        const newTab = await newTabPromise
-        await newTab.waitForLoadState()
+        const newTab = await newTabPromise;
+        await newTab.waitForLoadState();
         await expect(
           newTab.getByRole('heading', { name: 'Join LinkedIn' }),
-        ).toBeVisible()
+        ).toBeVisible();
       },
-    }
+    };
   }
 }
